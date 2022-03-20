94c8832d62950e361eb94a5a740e601717624547
why <488009667@qq.com>
2022-03-20 16:02:17 +0800

    搭建后端项目框架

---
+++ list-web/src/main/java/com/why/worklistserver/WorkListServerApplication.java
@@ -0,0 +1,17 @@
+package com.why.worklistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName：WorkListServerApplication
+ * @Description：todo springboot 启动类
+ * @Author: why
+ * @DateTime：2022/3/20 15:41
+ */
+@SpringBootApplication
+public class WorkListServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkListServerApplication.class,args);
+    }
+}
